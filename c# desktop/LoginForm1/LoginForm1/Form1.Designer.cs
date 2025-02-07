fb028e5bbca7979e89e8cfdce75b31d1c198b269
ildlt <32450760+ildlt@users.noreply.github.com>
2025-02-07 11:05:41 +0300

    Add files via upload

---
+++ c# desktop/LoginForm1/LoginForm1/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace LoginForm1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnToLogin = new System.Windows.Forms.Button();
+            this.btnToRegister = new System.Windows.Forms.Button();
+            this.panelLogin = new System.Windows.Forms.Panel();
+            this.panelRegister = new System.Windows.Forms.Panel();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.btnSignup = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSignin = new System.Windows.Forms.Button();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.panelLogin.SuspendLayout();
+            this.panelRegister.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnToLogin
+            // 
+            this.btnToLogin.Location = new System.Drawing.Point(149, 33);
+            this.btnToLogin.Name = "btnToLogin";
+            this.btnToLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnToLogin.TabIndex = 0;
+            this.btnToLogin.Text = "Login";
+            this.btnToLogin.UseVisualStyleBackColor = true;
+            this.btnToLogin.Click += new System.EventHandler(this.btnToLogin_Click);
+            // 
+            // btnToRegister
+            // 
+            this.btnToRegister.Location = new System.Drawing.Point(535, 33);
+            this.btnToRegister.Name = "btnToRegister";
+            this.btnToRegister.Size = new System.Drawing.Size(75, 23);
+            this.btnToRegister.TabIndex = 1;
+            this.btnToRegister.Text = "Register";
+            this.btnToRegister.UseVisualStyleBackColor = true;
+            this.btnToRegister.Click += new System.EventHandler(this.btnToRegister_Click);
+            // 
+            // panelLogin
+            // 
+            this.panelLogin.Controls.Add(this.panelRegister);
+            this.panelLogin.Controls.Add(this.btnSignin);
+            this.panelLogin.Controls.Add(this.textBoxPassword);
+            this.panelLogin.Controls.Add(this.textBoxUsername);
+            this.panelLogin.Controls.Add(this.labelPassword);
+            this.panelLogin.Controls.Add(this.labelUsername);
+            this.panelLogin.Location = new System.Drawing.Point(12, 88);
+            this.panelLogin.Name = "panelLogin";
+            this.panelLogin.Size = new System.Drawing.Size(776, 350);
+            this.panelLogin.TabIndex = 2;
+            // 
+            // panelRegister
+            // 
+            this.panelRegister.Controls.Add(this.textBoxEmail);
+            this.panelRegister.Controls.Add(this.labelEmail);
+            this.panelRegister.Controls.Add(this.btnSignup);
+            this.panelRegister.Controls.Add(this.textBox1);
+            this.panelRegister.Controls.Add(this.textBox2);
+            this.panelRegister.Controls.Add(this.label1);
+            this.panelRegister.Controls.Add(this.label2);
+            this.panelRegister.Location = new System.Drawing.Point(0, 0);
+            this.panelRegister.Name = "panelRegister";
+            this.panelRegister.Size = new System.Drawing.Size(776, 350);
+            this.panelRegister.TabIndex = 3;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(369, 124);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEmail.TabIndex = 6;
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(315, 127);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(35, 13);
+            this.labelEmail.TabIndex = 5;
+            this.labelEmail.Text = "E-mail";
+            // 
+            // btnSignup
+            // 
+            this.btnSignup.Location = new System.Drawing.Point(323, 249);
+            this.btnSignup.Name = "btnSignup";
+            this.btnSignup.Size = new System.Drawing.Size(75, 23);
+            this.btnSignup.TabIndex = 4;
+            this.btnSignup.Text = "Sign Up";
+            this.btnSignup.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(369, 182);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(369, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(300, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(300, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Username";
+            // 
+            // btnSignin
+            // 
+            this.btnSignin.Location = new System.Drawing.Point(326, 223);
+            this.btnSignin.Name = "btnSignin";
+            this.btnSignin.Size = new System.Drawing.Size(75, 23);
+            this.btnSignin.TabIndex = 4;
+            this.btnSignin.Text = "Sign In";
+            this.btnSignin.UseVisualStyleBackColor = true;
+            this.btnSignin.Click += new System.EventHandler(this.btnSignin_Click);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(369, 134);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPassword.TabIndex = 3;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(369, 64);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(100, 20);
+            this.textBoxUsername.TabIndex = 2;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(300, 137);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(53, 13);
+            this.labelPassword.TabIndex = 1;
+            this.labelPassword.Text = "Password";
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(300, 67);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(55, 13);
+            this.labelUsername.TabIndex = 0;
+            this.labelUsername.Text = "Username";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelLogin);
+            this.Controls.Add(this.btnToRegister);
+            this.Controls.Add(this.btnToLogin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelLogin.ResumeLayout(false);
+            this.panelLogin.PerformLayout();
+            this.panelRegister.ResumeLayout(false);
+            this.panelRegister.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnToLogin;
+        private System.Windows.Forms.Button btnToRegister;
+        private System.Windows.Forms.Panel panelLogin;
+        private System.Windows.Forms.Panel panelRegister;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.Button btnSignup;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSignin;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Label labelUsername;
+    }
+}
+
---
+++ c# desktop/LoginForm1/LoginForm1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginForm1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToLogin_Click(object sender, EventArgs e)
+        {
+            panelRegister.Visible = false;
+            panelLogin.Visible = true;
+            panelLogin.BringToFront();
+        }
+
+        private void btnToRegister_Click(object sender, EventArgs e)
+        {
+            panelLogin.Visible = false;
+            panelRegister.Visible = true;
+            panelRegister.BringToFront();
+        }
+
+        private void btnSignin_Click(object sender, EventArgs e)
+        {
+            DB db = new DB();
+            String username = textBoxUsername.Text;
+            String password = textBoxPassword.Text;
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `users` WHERE `username` = @usn AND `password` = @pass", db.getConnection());
+
+            command.Parameters.Add("@usn", MySqlDbType.VarChar).Value = username;
+            command.Parameters.Add("@pass", MySqlDbType.VarChar).Value = password;
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if(table.Rows.Count > 0)
+            {
+                MessageBox.Show("Yes");
+            } else
+            {
+                MessageBox.Show("No");
+            }
+        }
+
+    }
+}
